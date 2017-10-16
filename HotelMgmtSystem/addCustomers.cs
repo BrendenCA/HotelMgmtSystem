bed1dc5d559be6ae7307604f16e371218a780eca
Brenden <Brenden@LENOVO>
2017-10-16 21:50:02 +0530

    Implement adding customers

--- HotelMgmtSystem/addCustomers.cs
+++ HotelMgmtSystem/addCustomers.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
 
 namespace HotelMgmtSystem
 {
@@ -26,5 +27,29 @@ namespace HotelMgmtSystem
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dbconnect dbms = new dbconnect();
+            dbms.connect();
+            OracleCommand cmd = new OracleCommand("INSERT INTO CUSTOMER (NAME, ADDRESS, DATE_OF_BIRTH, AADHAR_NO, MOBILE_NO, EMAIL_ID) VALUES (:p1, :p2, :p3, :p4, :p5, :p6)", dbms.con);
+            cmd.Parameters.Add("p1", name.Text);
+            cmd.Parameters.Add("p2", address.Text);
+            cmd.Parameters.Add("p3", dob.Value.Date);
+            cmd.Parameters.Add("p4", aadharNo.Text);
+            cmd.Parameters.Add("p5", mobileNo.Text);
+            cmd.Parameters.Add("p6", email.Text);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Customer added successfully", "Success");
+                ((Form)this.TopLevelControl).Close();
+            }
+            catch(Exception exp)
+            { 
+                MessageBox.Show("Customer not added\n" + exp.Message, "Error!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
     }
 }
